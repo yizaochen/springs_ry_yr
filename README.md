bb0c191904f4734470553387c53f0015289b3bc3
yizaochen <yizaochen@gmail.com>
2021-11-06 18:02:12 +0800

    Update

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # springs_ry_yr
+
+# Create a virtual environment
+`conda create --name springs_ry_yr python=3.8`
+
+# Activate virtual environment
+`conda activate springs_ry_yr`
+
+# Deactivate
+`conda deactivate`
+
+# Install springs_ry_yr package
+`pip install -e .`
+
+# Upgrade package
+`pip install -e . --upgrade`
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='spring_ry_yr', 
+      version='0.1',
+      packages=find_packages(),
+      url='https://github.com/yizaochen/springs_ry_yr.git',
+      author='Yizao Chen',
+      author_email='yizaochen@gmail.com',
+      license='MIT',
+      install_requires=[
+          'matplotlib'
+      ]
+      )
